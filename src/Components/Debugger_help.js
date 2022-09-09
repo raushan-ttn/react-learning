5263e1d9912fb23dedbf2a275040b066ee99de57
Raushan Tiwari <raushan.tiwari@tothenew.com>
2022-09-09 23:15:00 +0530

    code update

---
+++ src/Components/Debugger_help.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Printstar = () => {
+    let result = "";
+    //debugger;
+    for(let i = 0; i < 5; i++){
+        result += "*";
+    }
+    return result;
+}
+
+export default Printstar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Printstar from "../Components/Debugger_help";
 
 const Home = () => {
     return (
+        <>
         <h1>This is our homepage.</h1>
+         <Printstar />
+        </>
     )
 }
 
