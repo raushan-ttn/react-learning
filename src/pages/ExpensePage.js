25c4c9ad49d6b008628f7a3f6d6bf04de69847bf
Raushan Tiwari <raushan.tiwari@tothenew.com>
2022-09-15 20:35:55 +0530

    code update

--- src/pages/ExpensePage.js
+++ src/pages/ExpensePage.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ExpenseItem from "../Components/self-learn/Expense/ExpenseItem";
 import Card from "../Components/self-learn/UI/Card";
 import NewExpense from "../Components/self-learn/NewExpense/NewExpense";
-
+import FilterExpense from "../Components/self-learn/NewExpense/FilterExpense";
 const ExpensePage = () => {
     const expenses = [
         {
@@ -25,8 +25,10 @@ const ExpensePage = () => {
             date: new Date(2022, 5, 12),
         },
     ];
-
+    // Define state to manage filters.
     const [newlyAddedExpense, newlyAddedExpenseSet] = useState(expenses);
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const [filteredExpense, filteredExpenseSet] = useState(newlyAddedExpense);
 
     //call data from child to parent reference.
     const saveNewExpenseHandler = (enteredNewExpense) => {
@@ -35,22 +37,57 @@ const ExpensePage = () => {
             id: Math.random().toString(),
         };
         // Set data in state. for use in another component.
-        newlyAddedExpenseSet([
-            ...newlyAddedExpense,
-            expenseData
-        ]);
-    }
+        /*         newlyAddedExpenseSet([
+                    ...newlyAddedExpense,
+                    expenseData
+                ]); */
+        // Another way to update state.
+        newlyAddedExpenseSet((prevExpenses) => {  // we can use anything inplace of prevExpenses just variable name.
+            return [expenseData, ...prevExpenses];
+        });
+
+        // Add newly added data in list with filter without page refresh.
+        const newAddedData =[expenseData, ...newlyAddedExpense];
+        const filteredExpenseData = newAddedData.filter(expense1 => {
+            return expense1.date.getFullYear().toString() == filteredYear;
+        });
+        filteredExpenseSet(filteredExpenseData);
+    };
+
+    // print date as string.
+    //console.log(expenses[0].date.toISOString());
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+        // Create copy of item list
+        // Include all elements which includes the search query
+        let filteredExpenseData = [...newlyAddedExpense];
+        filteredExpenseData = filteredExpenseData.filter(expense => {
+            return expense.date.getFullYear().toString() == selectedYear;
+        })
+        // Trigger render with updated values
+        filteredExpenseSet(filteredExpenseData);
+    };
+
+    // Render list with default value.
+    useEffect(() => {
+        const filteredExpenseData = filteredExpense.filter(expense1 => {
+            return expense1.date.getFullYear().toString() == filteredYear;
+        });
+        filteredExpenseSet(filteredExpenseData);
+    }, []);
+
+
+    //     filteredExpenseSet(filteredExpense);
 
     //Dynamic pass props in component.
-    const MyExpenses = newlyAddedExpense.map((expense) => {
+    const MyExpenses = filteredExpense.map((expense) => {
         return (
             <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
         );
     });
 
     // console.log(newlyAddedExpense,'newlyAddedExpense');
-    // print data as string.
-    //console.log(expenses[0].date.toISOString());
     return (
         <>
             <NewExpense onSaveNewExpense={saveNewExpenseHandler} />
@@ -60,6 +97,7 @@ Same as we can create Own attribute (onSaveNewExpense (we can use any name here)
              */}
             <Card className="expenses">
                 <h1>Expense Items are here:</h1>
+                <FilterExpense selected={filteredYear} onChangeFilter={filterChangeHandler} />
                 {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
             <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
             <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} /> */}
