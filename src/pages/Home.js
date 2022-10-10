c7bfad12aae58b3da1c6f63ea7460e1b121ed051
Raushan Tiwari <raushan.tiwari@tothenew.com>
2022-10-10 18:51:31 +0530

    login component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import Printstar, { AnotherPortals, AddUserForm } from "../Components/Debugger_help";
 import Wrapper from "../Helper/Wrapper";
 
+import MainHeader from "../Helper/Wrapper/MainHeader/MainHeader";
+import Login from "../Helper/Login/Login";
+import Home1 from "../Helper/Home/Home";
+
 const Home = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const loginHandler = (email, password) => {
+        // We should of course check email and password
+        // But it's just a dummy/ demo anyways
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        setIsLoggedIn(false);
+    };
+
     return (
         <Wrapper>
             {/* We can create custom wrapper components like Wrapper  Or we can use Fragments <> </> (blank tag)*/}
@@ -14,7 +31,15 @@ But above method will not create, see UserListData and NoPageData component. */}
             {/* Define  "start": "PORT=3006  react-scripts start", in package.json to run multiple project in local. */}
             <Printstar />
             <AddUserForm />
-            {ReactDOM.createPortal(<AnotherPortals />, document.getElementById("another_root"))};
+            {ReactDOM.createPortal(<AnotherPortals />, document.getElementById("another_root"))}
+            <h2> Login component</h2>
+            <React.Fragment>
+                <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+                <main>
+                    {!isLoggedIn && <Login onLogin={loginHandler} />}
+                    {isLoggedIn && <Home1 onLogout={logoutHandler} />}
+                </main>
+            </React.Fragment>
         </Wrapper>
     )
 }
