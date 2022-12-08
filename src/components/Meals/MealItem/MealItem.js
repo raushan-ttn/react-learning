4ea9d1526fb301c0be74ee7d7444c32e795a0a00
Raushan Tiwari <raushan.tiwari@tothenew.com>
2022-12-08 17:24:33 +0530

    input field

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,14 +1,17 @@
 import classes from "./MealItem.module.css";
-
+import MealItemForm
+  from "./MealItemForm";
 const MealItem = props => {
   const price = `$${props.price.toFixed(2)}`; // first $ for display, and second $ to pass dynamic value in template litterals (``)
   return <li className={classes.meal}>
     <div>
       <h3>{props.name}</h3>
       <div className={classes.description}>{props.description}</div>
-      <div className={classes.price}>{ price}</div>
+      <div className={classes.price}>{price}</div>
     </div>
-    <div></div>
+    <div>
+      <MealItemForm />
+    </div>
   </li>
 }
 
