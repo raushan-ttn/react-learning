bbffe5238062d3dc521cd315031c593767dae98e
Raushan Tiwari <raushan.tiwari@tothenew.com>
2022-12-09 20:13:54 +0530

    add modal popup using portals

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 
 
@@ -10,18 +11,20 @@ const Cart = props => {
     </ul>
   );
 
-  return <div>
-    {cartItems}
-    <div className={classes.total}>
-      <span>Total Amount</span>
-      <span>35.62</span>
-    </div>
-    <div className={classes.action}>
-      <button className={classes['button--alt']}>Close</button>
-      <button className={classes.button}>Order</button>
-    </div>
-    <div></div>
-  </div>
-}
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+      <div></div>
+    </Modal>
+  );
+};
 
 export default Cart;
